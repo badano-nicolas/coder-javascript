057d672184bf2d46e3e8d37282947e969de5b689
badano-nicolas <badano.nicolas@gmail.com>
2022-08-02 13:48:54 -0300

    Added player image

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const canvas = document.querySelector('canvas');
 
 const canvasContext = canvas.getContext('2d');
 
-
-
 canvas.width = 1024;
 canvas.height = 576;
 
@@ -13,9 +11,10 @@ canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 const mapImage = new Image();
 mapImage.src = './assets/img/towns/town-0.png';
 
+const playerImage = new Image();
+playerImage.src = './assets/img/chars/main/down.png'
 
 mapImage.onload = () => {
-    canvasContext.drawImage(mapImage, 0, 0);
+    canvasContext.drawImage(mapImage, -570, -160);
+    canvasContext.drawImage(playerImage, 0, 0);
 }
-
-
