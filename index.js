e0696e50c44ff474be3f76b16aaa798a4d2ed10a
badano-nicolas <badano.nicolas@gmail.com>
2022-08-02 14:13:54 -0300

    Player image in correct posssition, added event listener

--- index.js
+++ index.js
@@ -15,6 +15,39 @@ const playerImage = new Image();
 playerImage.src = './assets/img/chars/main/down.png'
 
 mapImage.onload = () => {
-    canvasContext.drawImage(mapImage, -570, -160);
-    canvasContext.drawImage(playerImage, 0, 0);
+    canvasContext.drawImage(mapImage, -570, -165);
+    canvasContext.drawImage(playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - playerImage.width / 4,
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height
+    )
 }
+
+window.addEventListener('keydown', (e) => {
+    detectKeyDown(e);
+})
+
+const detectKeyDown = (event) => {
+    const key = event.key;
+    switch (key) {
+        case "w":
+            console.log("pressed w");
+            break;
+        case "s":
+            console.log("pressed s");
+            break;
+        case "d":
+            console.log("pressed d");
+            break;
+        case "a":
+            console.log("pressed a");
+            break;
+        default:
+            break;
+    }
+};
