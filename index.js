e89041b31fc29ffea88e493b4fe0822aa8d0a29b
badano-nicolas <badano.nicolas@gmail.com>
2022-08-03 00:09:07 -0300

    Colliding working

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ canvas.height = 576;
 
 const collisionsMap = [];
 
-// Parse collision in rows of 70 columns
+// parse collision in rows of 70 columns
 for (let i = 0; i < collisions.length; i += 70) {
     collisionsMap.push(collisions.slice(i, 70 + i));
 }
@@ -56,16 +56,45 @@ const playerImage = new Image();
 playerImage.src = './assets/img/chars/main/down.png'
 
 class Sprite {
-    constructor({ position, velocity, image }) {
+    constructor({ position, velocity, image, frames = { max: 1 } }) {
         this.position = position;
         this.image = image;
+        this.frames = frames;
+
+        this.image.onload = () => {
+            this.width = this.image.width / this.frames.max;
+            this.height = this.image.height;
+        };
+
     }
 
     draw() {
-        canvasContext.drawImage(this.image, this.position.x, this.position.y);
+        canvasContext.drawImage(
+            this.image,
+            0,
+            0,
+            this.image.width / this.frames.max,
+            this.image.height,
+            this.position.x,
+            this.position.y,
+            this.image.width / this.frames.max,
+            this.image.height
+        )
     }
 }
 
+const player = new Sprite({
+    position: {
+        // 192 is the width of the player image, can't wait to load the image to obtain
+        x: canvas.width / 2 - 192 / 4,
+        // 68  is the height of the player image, can't wait to load the image to obtain
+        y: canvas.height / 2 - 68 / 2
+    },
+    image: playerImage,
+    frames: {
+        max: 4
+    }
+})
 
 const background = new Sprite({
     position: {
@@ -97,36 +126,42 @@ const testBoundary = new Boundary({
     }
 });
 
+
+// Creted this variable to simplify later
+const movables = [background, testBoundary]
+
 function animate() {
     window.requestAnimationFrame(animate);
+
     background.draw();
 
-    boudaries.forEach(boundary => {
-        boundary.draw();
-    });
-    //testBoundary.draw();
-    canvasContext.drawImage(playerImage,
-        0,
-        0,
-        playerImage.width / 4,
-        playerImage.height,
-        canvas.width / 2 - playerImage.width / 4,
-        canvas.height / 2 - playerImage.height / 2,
-        playerImage.width / 4,
-        playerImage.height
-    )
+    testBoundary.draw();
+    player.draw();
+
+    if (player.position.x + player.width >= testBoundary.position.x) {
+        console.log('colliding')
+
+    }
 
     if (keys.w.pressed && lastKey === 'w') {
-        background.position.y += 3;
+        movables.forEach(movable => {
+            movable.position.y += 3;
+        });
     }
     else if (keys.a.pressed && lastKey === 'a') {
-        background.position.x += 3;
+        movables.forEach(movable => {
+            movable.position.x += 3;
+        });
     }
     else if (keys.s.pressed && lastKey === 's') {
-        background.position.y -= 3;
+        movables.forEach(movable => {
+            movable.position.y -= 3;
+        });
     }
     else if (keys.d.pressed && lastKey === 'd') {
-        background.position.x -= 3;
+        movables.forEach(movable => {
+            movable.position.x -= 3;
+        });
     }
 }
 
