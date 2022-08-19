c2b59a44a576d95b914be0c0aa09a173d7e07968
badano-nicolas <badano.nicolas@gmail.com>
2022-08-19 00:42:45 -0300

    Refactor classes

--- src/game/game.js
+++ src/game/game.js
@@ -11,21 +11,6 @@ for (let i = 0; i < collisions.length; i += 70) {
     collisionsMap.push(collisions.slice(i, 70 + i));
 }
 
-class Boundary {
-    static width = 48;
-    static height = 48;
-    constructor({ position }) {
-        this.position = position;
-        this.width = 48;
-        this.height = 48;
-    }
-
-    draw() {
-        canvasContext.fillStyle = 'rgba(255, 0, 0, 0.0)';
-        canvasContext.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
-}
-
 const boudaries = [];
 
 const offset = {
@@ -55,34 +40,6 @@ mapImage.src = './assets/img/towns/town-0.png';
 
 const playerImage = new Image();
 playerImage.src = './assets/img/chars/main/down.png'
-
-class Sprite {
-    constructor({ position, velocity, image, frames = { max: 1 } }) {
-        this.position = position;
-        this.image = image;
-        this.frames = frames;
-
-        this.image.onload = () => {
-            this.width = this.image.width / this.frames.max;
-            this.height = this.image.height;
-        };
-
-    }
-
-    draw() {
-        canvasContext.drawImage(
-            this.image,
-            0,
-            0,
-            this.image.width / this.frames.max,
-            this.image.height,
-            this.position.x,
-            this.position.y,
-            this.image.width / this.frames.max,
-            this.image.height
-        )
-    }
-}
 
 const player = new Sprite({
     position: {
